8bd782bf477c349e418d7fad2db272e8d7cac18a
Alexander Yuriev <alexyuriev@users.noreply.github.com>
2018-01-04 18:12:45 -0500

    Add screen blanking using a full size X window
    * a black window stretches over entire workspace including multiple displays
    * a window manager decorations are turned off on this window ( Tested in OpenBox )
    * cursor is turned off

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ terminal locked. I've had many people just go to my screen and hit tons of keys
 just to make it crash, or cycling the screen saver to get to a certain one
 known to crash. Absolutly ridiculous!
 
-This is my solution to the problem. Very simple, very small, very few features.
-Doesn't even blank your screen, all this program does is lock your keyboard and
-mouse, and only unlocks it if you type your login password and hit return.
+This is my solution to the problem. Very simple, very small, very few
+features.  The program blanks the screen (creates a black window) and turns
+off the cursor, locks your keyboard and mouse, and only unlocks it if you
+type your login password and hit return.
 
 You can also override the password by specifying it as the first argument of
 the command line. A third way is to set the XLPASSWD environment variable.
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
  *  Danny Dulai
  *
  *  Simple, transparent screen locker in X11
+ *  Screen blanking added by Alex Yuriev <github.com/alexyuriev>
  */
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -21,9 +22,9 @@ int
 main(int argc, char *argv[])
 {
     Display *display;
-    Window root;
-    int len = 0, i;
     XEvent ev;
+
+    int len = 0, i;
     char keybuffer[1024], *passwd;
 
     if (argc > 1) {
@@ -39,54 +40,89 @@ main(int argc, char *argv[])
 	passwd = getpwuid(getuid())->pw_passwd;
 #else
 	{
-	    struct spwd *sp = getspnam(getpwuid(getuid())->pw_name);
-	    if (sp)
-		passwd = sp->sp_pwdp;
-	    else {
-		fprintf(stderr, "could not get shadow entry\n");
-		exit(1);
-	    }
-	}
+		struct spwd *sp = getspnam(getpwuid(getuid())->pw_name);
+		if (sp)
+			passwd = sp->sp_pwdp;
+		else {
+			fprintf(stderr, "could not get shadow entry\n");
+			exit(1);
+		}
+    }
 #endif
-	if (!passwd) {
-	    fprintf(stderr, "could not get passwd\n");
-	    exit(1);
-	}
+    if (!passwd) {
+	fprintf(stderr, "could not get passwd\n");
+    exit(1);
+    }
     }
 
     if (strlen(passwd) < 2) {
-	fprintf(stderr, "passwd too short\n");
-	exit(1);
+    fprintf(stderr, "passwd too short\n");
+    exit(1);
     }
 
     if ((display = XOpenDisplay(NULL)) == NULL) {
-	fprintf(stderr, "could not connect to $DISPLAY\n");
-	exit(1);
+    fprintf(stderr, "could not connect to $DISPLAY\n");
+    exit(1);
     }
 
-    root = DefaultRootWindow(display);
+    Window root = DefaultRootWindow(display);
+    if ( root < 0 ) {
+        fprintf(stderr, "Failed to get a root window ID of a default screen\n");
+        exit(1);
+    }
+
+    int screen = DefaultScreen( display );
+
+
+    XWindowAttributes xwRootWinAttr;
+    Status statusRet = XGetWindowAttributes( display, root, &xwRootWinAttr );
+
+    Window w = XCreateSimpleWindow( display, root, 0, 0, xwRootWinAttr.width, xwRootWinAttr.height, 0, 0, BlackPixel(display, screen) );
+
+    XColor black;
+    black.red   = 0;
+    black.green = 0;
+    black.blue  = 0;
+
+    static char noData[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    Pixmap emptyBitMap = XCreateBitmapFromData( display, w, noData, 8, 8);
+
+    Cursor invisibleCursor = XCreatePixmapCursor( display, emptyBitMap, emptyBitMap, &black, &black, 0, 0);
+    XDefineCursor( display, w, invisibleCursor );
+    XFreeCursor( display, invisibleCursor );
+    XFreePixmap( display, emptyBitMap );
+
+    /* Attributes for the locked window */
+
+    XSetWindowAttributes xwWindowSetAttr;
+    xwWindowSetAttr.save_under        = True;
+    xwWindowSetAttr.override_redirect = True;
+
+    XChangeWindowAttributes( display, w, CWOverrideRedirect | CWSaveUnder, &xwWindowSetAttr );
+    XMapWindow( display, w);
+
     XGrabPointer(display, root, 1, ButtonPress, GrabModeAsync, GrabModeAsync, None, None, CurrentTime);
     XGrabKeyboard(display, root, 0, GrabModeAsync, GrabModeAsync, CurrentTime);
     XSelectInput(display, root, KeyPressMask);
 
     while (XNextEvent(display, &ev), 1) {
-	if (ev.type == KeyPress) {
-	    KeySym keysym;
-	    XComposeStatus compose;
-	    if (len > sizeof(keybuffer)-10)
-		len = 0;
-	    i = XLookupString(&ev.xkey, keybuffer+len, 10, &keysym, &compose);
+    if (ev.type == KeyPress) {
+        KeySym keysym;
+        XComposeStatus compose;
+        if (len > sizeof(keybuffer)-10)
+        len = 0;
+        i = XLookupString(&ev.xkey, keybuffer+len, 10, &keysym, &compose);
 
-	    if (keysym == XK_Return) {
-		keybuffer[len] = 0;
-		if (len && !strcmp(crypt(keybuffer, passwd), passwd)) {
-		    XUngrabKeyboard(display, CurrentTime);
-		    XUngrabPointer(display, CurrentTime);
-		    exit(0);
-		} else
-		    len = 0;
-	    } else
-		len += i;
-	}
+        if (keysym == XK_Return) {
+        keybuffer[len] = 0;
+        if (len && !strcmp(crypt(keybuffer, passwd), passwd)) {
+            XUngrabKeyboard(display, CurrentTime);
+            XUngrabPointer(display, CurrentTime);
+            exit(0);
+        } else
+            len = 0;
+        } else
+        len += i;
+    }
     }
 }
